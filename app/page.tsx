ba9145d8e60929dda0c9351f85766c2f974cbb6e
Thanarat Ruengkum <ninenights19534@gmail.com>
2024-11-08 22:53:40 +0700

    Add center box and title

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,15 @@ import React, { useState, useEffect } from "react";
 import { Box, Center, Group, Stack, Text, Title } from "@mantine/core";
 
 export default function Home() {
-  return <Text>Hello World</Text>;
+  return (
+    <Group style={{ backgroundColor: "#9c36b5", height: "100vh" }}>
+      <Center
+        h={100}
+        bg={"var(--mantine-color-white)"}
+        style={{ borderRadius: "10px", padding: "35vh", margin: "auto" }}
+      >
+        <Title order={1}>Todo List</Title>
+      </Center>
+    </Group>
+  );
 }
