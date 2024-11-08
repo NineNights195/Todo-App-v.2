79a22a8fc378c1a9eb9b09c70d060bc5d3f87d52
Thanarat Ruengkum <ninenights19534@gmail.com>
2024-11-08 22:49:09 +0700

    Setup manteen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import { Box, Center, Group, Stack, Text, Title } from "@mantine/core";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <Text>Hello World</Text>;
 }
